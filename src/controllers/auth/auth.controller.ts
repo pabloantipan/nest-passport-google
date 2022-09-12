4f29857067c9abe410d24e761fcc15ee35de9bb9
Pablo Antipan Q <antipan.pablo@gmail.com>
2022-09-11 23:59:45 -0300

    add authGuard

--- src/controllers/auth/auth.controller.ts
+++ src/controllers/auth/auth.controller.ts
@@ -1,13 +1,29 @@
+import { CurrentUser } from '@decorators/current-user.decorator';
 import { Serialize } from '@decorators/serialize.decorator';
 import { CreateUserDto } from '@dtos/users/create-user.dto';
 import { UserDto } from '@dtos/users/user.dto';
+import { AuthGuard } from '@guards/auth.guard';
 import { AuthLogic } from '@logics/auth/auth.logic';
-import { Body, Controller, Post, Session } from '@nestjs/common';
+import { User } from '@models/user';
+import {
+  Body,
+  Controller,
+  Get,
+  Post,
+  Session,
+  UseGuards,
+} from '@nestjs/common';
 
 @Controller('/auth')
 @Serialize(UserDto)
 export class AuthController {
   constructor(private authLogic: AuthLogic) {}
+
+  @Get('whoami')
+  @UseGuards(AuthGuard)
+  async whoAmI(@CurrentUser() user: User) {
+    return user;
+  }
 
   @Post('/signup')
   public async createUser(
