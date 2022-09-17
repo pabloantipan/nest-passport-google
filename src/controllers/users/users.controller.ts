1433235163cf36991df0816e22ad7f1667295071
Pablo Antipan Q <antipan.pablo@gmail.com>
2022-09-17 19:08:25 -0300

    fix desarializerUser function

--- src/controllers/users/users.controller.ts
+++ src/controllers/users/users.controller.ts
@@ -2,15 +2,18 @@ import { CurrentUser } from '@decorators/current-user.decorator';
 import { Serialize } from '@decorators/serialize.decorator';
 import { UserDto } from '@dtos/users/user.dto';
 import { AuthGuard } from '@guards/auth.guard';
+import { GoogleAuthGuard } from '@guards/google-auth.guard';
 import { User } from '@models/user';
 import {
   Controller,
   Get,
   NotFoundException,
   Param,
+  Req,
   UseGuards,
 } from '@nestjs/common';
 import { UsersService } from '@services/users/users.service';
+import { Request } from 'express';
 
 @Controller('users')
 @Serialize(UserDto)
@@ -28,6 +31,15 @@ export class UsersController {
     return user;
   }
 
+  @Get('status')
+  user(@Req() request: Request) {
+    if (request.user) {
+      return { msg: 'Authenticated' };
+    } else {
+      return { msg: 'Not Authenticated' };
+    }
+  }
+
   @Get('/:id')
   async findUser(@Param('id') id: string) {
     const user = await this.usersService.findOne(parseInt(id));
@@ -35,15 +47,5 @@ export class UsersController {
       throw new NotFoundException('user not found');
     }
     return user;
-  }
-
-  @Get('status')
-  @UseGuards(AuthGuard)
-  async user(@CurrentUser() user: User) {
-    if (user) {
-      return { msg: 'Authenticated', user: user };
-    } else {
-      return { msg: 'Not Authenticated', user: user };
-    }
   }
 }
